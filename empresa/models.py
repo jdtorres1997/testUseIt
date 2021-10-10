e9a6cad9bf7e94986e6a53cd1d09633eab110a3d
Juan Torres <juan.torres.canon@correounivalle.edu.co>
2021-10-10 08:09:34 -0500

    Inicial empresas

--- empresa/models.py
+++ empresa/models.py
@@ -6,13 +6,17 @@ class Empresa(models.Model):
     nombre = models.CharField(verbose_name='Nombre de la empresa',max_length=100)
     nombre_comercial = models.CharField(verbose_name='Nombre comercial de la empresa',max_length=100)
     direccion = models.CharField(verbose_name='Dirección',max_length=100)
-    telefono = models.PositiveIntegerField(verbose_name='Teléfono')
+    telefono = models.BigIntegerField(verbose_name='Teléfono')
     email = models.EmailField(verbose_name='Correo electrónico')
-    sitio_web = models.URLField(max_length=200)
+    sitio_web = models.URLField(max_length=200, verbose_name='Sitio Web')
     pais = models.CharField(verbose_name='País',max_length=100)
     estado = models.CharField(verbose_name='Departamento',max_length=100)
     ciudad = models.CharField(verbose_name='Ciudad',max_length=100)
-    propietario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    propietario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Propietario')
 
     def __str__(self):
         return "%s - %s" % (self.nit, self.nombre_comercial)
+    
+    def get_info():
+        empresas = Empresa.objects.order_by('id')
+        return empresas
--- empresa/views.py
+++ empresa/views.py
@@ -1,3 +1,76 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Empresa
+from django.contrib.auth.decorators import login_required
+from .forms import EmpresaAddForm, EmpresaEditForm
+from django.contrib import messages
 
 # Create your views here.
+@login_required
+def gestion_empresas(request):
+	# Usuario que hizo la peticion a la funcion (usuario que esta en la sesion)
+	usuario = request.user
+	return render(request, 'empresa/gestion.html',
+					{'empresas': Empresa.get_info()})
+
+@login_required
+def add_empresa(request):
+	usuario = request.user
+	print(usuario)
+	if request.method == 'POST':
+		form = EmpresaAddForm(request.POST)
+		if form.is_valid():
+			messages.success(request, 'Empresa registrado exitosamente')
+			empresa = form.save(commit=False)
+			empresa.propietario = usuario
+			empresa.save()
+			return redirect('empresa:gestion')
+		else:
+			messages.error(request, 'Por favor corrige los errores')
+			return render(request, 'empresa/add.html', {'form': form})
+	else:
+		form = EmpresaAddForm()
+		return render(request, 'empresa/add.html',
+					{'form': form})
+
+@login_required
+def editar_empresa(request, id_empresa):
+	empresa = Empresa.objects.get(id=id_empresa)
+	usuario = request.user
+	print(empresa)
+	if request.method == 'POST':
+		form = EmpresaEditForm(request.POST, instance=empresa)
+		if form.is_valid():
+			messages.success(request, 'Empresa registrado exitosamente')
+			form.save()
+			return redirect('empresa:gestion')
+		else:
+			messages.error(request, 'Por favor corrige los errores')
+			return render(request, 'empresa/edit.html', {'form': form})
+	else:
+		form = EmpresaEditForm(instance=empresa)
+		return render(request, 'empresa/edit.html',
+					{'form': form})
+
+@login_required
+def eliminar_empresa(request, id_empresa):
+	empresa = Empresa.objects.get(id=id_empresa)
+	usuario = request.user
+
+	if True:
+		if request.method == 'POST':
+			
+			empresa.delete()
+			messages.success(request, 'Has eliminado la empresa exitosamente!')
+			return redirect('empresa:gestion')
+		else:
+			return render(request, 'empresa/delete.html', {'empresa': empresa})
+	else:
+		messages.error(request, 'No estas autorizado para realizar esta acción')
+		return redirect('empresa:gestion')
+
+@login_required
+def detail_empresa(request, id_empresa):
+	empresa = Empresa.objects.get(id=id_empresa)
+	usuario = request.user
+
+	return render(request, 'empresa/detail.html', {'empresa': empresa})
