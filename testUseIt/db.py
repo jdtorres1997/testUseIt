a6ac65f08e7b27aa55ee2efbcedbe4fd49cbc21c
Juan Torres <juan.torres.canon@correounivalle.edu.co>
2021-10-11 18:55:53 -0500

    Requirements and DB conection

---
+++ testUseIt/db.py
@@ -0,0 +1,9 @@
+DBCONFIG = {}
+
+DBCONFIG['default'] = {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'testUseIt',
+    'USER': 'jdtorres',
+    'PASSWORD': 'DeV.2021',
+    'HOST': 'localhost',
+}
